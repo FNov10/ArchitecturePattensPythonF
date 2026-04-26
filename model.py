91925a3329f13660a909bf7502e5b736c330970e
Fahad Naveed <fahadnaveed@Fahads-MacBook-Air-2.local>
2026-04-26 18:43:20 -0400

    Added allocation, deallocation functionality. validated allo/deallo, orderlines are now stored in a set, and available quantity is now dynamically calculated based on the set allocated_lines

---
+++ model.py
@@ -0,0 +1,56 @@
+from dataclasses import dataclass
+from datetime import date
+from typing import Optional
+
+@dataclass(frozen=True)
+class OrderLine:
+    orderid: str
+    qty: int
+    sku: str
+
+class Batch:
+    def __init__(
+            self,
+            ref: str,
+            sku: str,
+            qty: int,
+            eta: Optional[date]
+    ):
+        self.reference = ref
+        self.sku = sku
+        self.eta = eta
+        self._purchased_qty = qty
+        self._allocated_lines = set()
+
+    def __str__(self):
+        return (f"Batch {self.sku} with ref {self.reference}, available quantity of"
+                f" {self.available_quantity}, and an ETA of {self.eta}")
+
+    def allocate(self, line: OrderLine) -> None:
+        if self.can_allocate(line):
+            self._allocated_lines.add(line)
+
+    def deallocate(self, line: OrderLine):
+        if self.can_deallocate(line):
+            self._allocated_lines.remove(line)
+
+
+    def can_allocate(self, line:OrderLine):
+        return all(
+            [
+                line.qty <= self.available_quantity,
+                self.sku == line.sku
+            ]
+        )
+    def can_deallocate(self, line:OrderLine):
+        return line in self._allocated_lines
+
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.qty for line in self._allocated_lines)
+
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_qty - self.allocated_quantity
+
+
--- test_model.py
+++ test_model.py
@@ -1,5 +1,6 @@
 from datetime import date, timedelta
 import pytest
+from model import *
 
 # from model import ...
 
@@ -7,21 +8,66 @@ today = date.today()
 tomorrow = today + timedelta(days=1)
 later = tomorrow + timedelta(days=10)
 
+def make_batch_and_line(sku: str, batch_qty: int, line_qty:int):
+    return (
+        Batch("batch_001", sku, batch_qty, today),
+        OrderLine("order-001", line_qty, sku)
+    )
 
 def test_allocating_to_a_batch_reduces_the_available_quantity():
-    pytest.fail("todo")
+    test_batch = Batch("batch_001", "BigTable", 30, later)
+    test_orderline = OrderLine(
+        "sample-order",
+        6,
+        "BigTable",
+    )
+    print(f"Assigning orderline of {test_orderline.qty} to {test_batch}")
+    test_batch.allocate(test_orderline)
+    print(f"Batch is now: {test_batch}")
+    assert test_batch.available_quantity == 24
 
 
 def test_can_allocate_if_available_greater_than_required():
-    pytest.fail("todo")
+    test_batch, test_orderline = make_batch_and_line(
+        "Lamp", 30,  20
+    )
+    assert test_batch.can_allocate(test_orderline)
 
 
 def test_cannot_allocate_if_available_smaller_than_required():
-    pytest.fail("todo")
+    test_batch, test_orderline = make_batch_and_line(
+        "Lamp", 30, 40
+    )
+    assert not test_batch.can_allocate(test_orderline)
 
 
 def test_can_allocate_if_available_equal_to_required():
-    pytest.fail("todo")
+    test_batch, test_orderline = make_batch_and_line(
+        "Lamp", 40, 40
+    )
+    assert test_batch.can_allocate(test_orderline)
+
+def test_cannot_allocate_if_skus_do_not_match():
+    test_batch = Batch("batch_001", "BigTable", 30, later)
+    test_orderline = OrderLine(
+        "sample-order",
+        6,
+        "BigChair",
+    )
+    assert test_batch.can_allocate(test_orderline) is False
+
+
+def test_can_only_deallocate_allocated_lines():
+    batch, unallocated_line = make_batch_and_line("TABLE", 40, 30)
+    batch.deallocate(unallocated_line)
+    assert batch.available_quantity == 40
+
+def test_allocation_is_impodent():
+    batch, line = make_batch_and_line("TABLE", 40, 30)
+    batch.allocate(line)
+    batch.allocate(line)
+    assert batch.available_quantity==10
+
 
 
 def test_prefers_warehouse_batches_to_shipments():
